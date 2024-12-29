e44019f556559273ec02c38e527033be1f7ba0a2
Quentin Adam <quentinadam@gmail.com>
2024-12-29 01:33:20 +0100

    Add documentation. Bump version to 0.1.9

--- src/assert.ts
+++ src/assert.ts
@@ -1,10 +1,24 @@
+/**
+ * An error that is thrown by the {@linkcode assert} function.
+ */
 export class AssertionError extends Error {
+  /**
+   * Creates a new AssertionError.
+   */
   constructor(message = 'Assertion failed') {
     super(message);
     this.name = 'AssertionError';
   }
 }
 
+/**
+ * Asserts that a value is true.
+ *
+ * @param value The value to assert
+ * @param error The error to throw if the value is not true.
+ *   If not provided, an AssertionError will be thrown.
+ *   If a string is provided, it will be used as the error message for the {@linkcode AssertionError}.
+ */
 export default function assert(value: boolean, error?: Error | string): asserts value {
   if (value !== true) {
     if (error === undefined) {
